2eeaa9f14dfe1d96fce80d2e1268ef629bb6185f
simonemassenti <s.massenti96@gmail.com>
2023-11-23 17:14:34 +0100

    add: Main

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -2,16 +2,50 @@
 export default {
     data() {
         return {
-
+            buyMenu: [
+                {
+                    name: "digital comics",
+                    image: "comics-digital-comics.png"
+                },
+                {
+                    name: "dc merchandise",
+                    image: "comics-merchandise.png"
+                },
+                {
+                    name: "subscription",
+                    image: "comics-subscriptions.png"
+                },
+                {
+                    name: "comic shop locator",
+                    image: "comics-shop-locator.png"
+                },
+                {
+                    name: "dc power visa",
+                    image: "dc-power-visa.svg"
+                }
+            ]
+        }
+    },
+    methods: {
+        getImagePath(img) {
+            return new URL(`../assets/img/buy-${img}`, import.meta.url).href;
         }
     }
 }
 </script>
 
 <template>
-
+<div class="main">
+    <ul>
+        <li v-for="item in buyMenu"> 
+            <img :src="getImagePath(item.image)" alt=""> 
+            <span>{{ item.name.toUpperCase() }}</span>
+        </li>
+    </ul>
+</div>
 </template>
 
-<style>
+<style scoped lang="scss">
+@use "../style/partials/main"
 
 </style>
